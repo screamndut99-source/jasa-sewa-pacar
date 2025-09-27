80df2f0ecbf4a720d6ee65ce58437d768d841930
screamndut99 <screamndut99@gmail.com>
2025-09-28 01:55:55 +0700

    Update project

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Data produk
+// Data produk, dengan array images untuk multi-gambar
 const products = [
   { 
     id: 1, 
     name: "SAMBO", 
-    image: "gambar/sambo.jpg", 
+    images: ["gambar/sambo.jpg", "gambar/sambo2.jpg", "gambar/sambo3.jpg", "gambar/sambo4.jpg", "gambar/sambo5.jpg", "gambar/sambo6.jpg", "gambar/agus.jpg"], 
     price: "$600", 
     description: "Mantan joki motor jalanan yang beralih passion menjadi seorang ahli slot. Murah senyum dan pandai merangkai sebuah pantun yang membuat hati para wanita meleleh seperti aspal kepanasan. Sambo yang sekarang sedang bekerja sebagai pemantau server pgsoft bergabung dengan kami untuk mencari kesenangan karena masalalu percintaannya yang kelam. Jangan lupa jika berkencan dengan Sambo panggil dia dengan panggilan Seng.", 
     telegram: "https://t.me/sambo111234" 
@@ -11,7 +11,7 @@ const products = [
   { 
     id: 2, 
     name: "KELIBO", 
-    image: "gambar/klibo.jpg", 
+    images: ["gambar/klibo.jpg", "gambar/klibo2.jpg", "gambar/klibo3.jpg", "gambar/klibo4.jpg", "gambar/klibo5.jpg", "gambar/agus.jpg"], 
     price: "R$999", 
     description: "Pria terkenal dengan sebutan kelibo ,dengan penampilan yang seadanya tetapi menarik di mata wanita karena wajahnya yang manis. Tetapi kisah cintanya yang tidak mulus, Kelibo memutuskan bergabung dengan Tim kami untuk mencari kesenangan bersama wanita-wanita yang ingin menyewa atau menjadikan dia pacar sewaan.", 
     telegram: "https://t.me/kelibo12" 
@@ -19,7 +19,7 @@ const products = [
   { 
     id: 3, 
     name: "IDAM", 
-    image: "gambar/idam.jpg", 
+    images: ["gambar/idam.jpg", "gambar/idam2.jpg", "gambar/idam3.jpg", "gambar/agus.jpg"], 
     price: "$650", 
     description: "Germo yang akrab dipanggil gendut karena badannya yang gemoy mirip dengan presiden kita Prabowo. Gendut diduga sebagai saudara kandung dari komedian ternama Indonesia si Adul, orangnya dikenal dengan humorisnya dan pandai memikat hati seorang oun khmer, membuat dugaan orang-orang terhadap dirinya sebagai saudara komedian Adul semakin terbukti.", 
     telegram: "https://t.me/Halamadridista15" 
@@ -36,10 +36,23 @@ function showProduct(id) {
     return;
   }
 
+  // Buat slide untuk setiap gambar
+  let slides = product.images.map(img => `
+    <div class="swiper-slide">
+      <img src="${img}" alt="${product.name}">
+    </div>
+  `).join("");
+
   productDetailDiv.innerHTML = `
     <div class="product-detail-container">
-      <div class="product-image">
-        <img src="${product.image}" alt="${product.name}">
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          ${slides}
+        </div>
+        <!-- Pagination & Navigation -->
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
       </div>
       <div class="product-info">
         <h2>${product.name}</h2>
@@ -58,24 +71,33 @@ function showProduct(id) {
   const addCartBtn = document.getElementById("addCart");
   addCartBtn.addEventListener("click", (e) => {
     const telegramLink = e.target.dataset.telegram;
-    alert(`Hubungi Telegram ${product.name}`);
     window.open(telegramLink, "_blank");
   });
 
-  // Event Next
-  const nextBtn = document.getElementById("next");
-  nextBtn.addEventListener("click", () => {
+  // Event Next / Previous
+  document.getElementById("next").addEventListener("click", () => {
     let nextId = id + 1;
     if (nextId > products.length) nextId = 1;
     window.location.href = `product.html?id=${nextId}`;
   });
 
-  // Event Previous
-  const prevBtn = document.getElementById("prev");
-  prevBtn.addEventListener("click", () => {
+  document.getElementById("prev").addEventListener("click", () => {
     let prevId = id - 1;
     if (prevId < 1) prevId = products.length;
     window.location.href = `product.html?id=${prevId}`;
+  });
+
+  // Inisialisasi Swiper
+  new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 }
 
